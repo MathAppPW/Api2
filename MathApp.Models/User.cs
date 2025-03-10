41e24f4ca85ce688fed239bad0c0481d96ee0698
Kamil <01178868@pw.edu.pl>
2025-03-10 17:05:02 +0100

    user profile update v2

--- MathApp.Models/User.cs
+++ MathApp.Models/User.cs
@@ -8,5 +8,12 @@ public class User
     [Required] public string Email { get; set; } = "";
     [Required] public string Username { get; set; } = "";
     [Required] public string PasswordHash { get; set; } = "";
+    [Required] public int Level { get; set; } = 1;
+    [Required] public int Experience { get; set; } = 0;
+    [Required] public int Streak { get; set; } = 0;
+    [Required] public int Lives { get; set; } = 10;
+    [Required] public DateTime LastLivesUpdate { get; set; } = DateTime.UtcNow;
+    [Required] public int RocketSkin { get; set; } = 0;
+    [Required] public int ProfileSkin { get; set; } = 0;
     public string? TokenVersion { get; set; } = "";
 }
