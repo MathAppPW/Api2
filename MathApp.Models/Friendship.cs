0a1f7f8dd579c689b48917d9e73e6173493442ae
Kamil <01178868@pw.edu.pl>
2025-04-08 10:58:46 +0200

    friendship id

--- MathApp.Models/Friendship.cs
+++ MathApp.Models/Friendship.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models;
 
 public class Friendship
 {
+    [Required]
+    public string Id { get; set; } = "";
     [ForeignKey(nameof(User1))] public string UserId1 { get; set; } = "";
     [ForeignKey(nameof(User2))] public string UserId2 { get; set; } = "";
     
