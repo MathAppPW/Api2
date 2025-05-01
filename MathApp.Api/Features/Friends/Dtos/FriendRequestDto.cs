05c1b08b5fc4b13a836adb27850a090e2a81a622
Kamil <01178868@pw.edu.pl>
2025-05-01 11:23:26 +0200

    Friend requests return id and sender avatar

--- MathApp.Api/Features/Friends/Dtos/FriendRequestDto.cs
+++ MathApp.Api/Features/Friends/Dtos/FriendRequestDto.cs
@@ -2,7 +2,9 @@ namespace MathAppApi.Features.Friends.Dto;
 
 public class FriendRequestDto
 {
+    public int Id { get; set; }
     public string SenderName { get; set; } = "";
     public string ReceiverName { get; set; } = "";
     public DateTime TimeStamp { get; set; }
+    public int AvatarSkinId { get; set; }
 }
