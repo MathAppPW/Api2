a5cf1754be4c06694c7a0424412dfbe2bd9d8aa5
s3r10us3r <tradeckimikolaj7@gmail.com>
2025-06-04 17:29:52 +0200

    Fixed ranking

--- MathApp.Api/Features/Rankings/Dtos/Ranking.cs
+++ MathApp.Api/Features/Rankings/Dtos/Ranking.cs
@@ -5,6 +5,7 @@ public class Ranking
     public List<RankingEntry> RankingEntries { get; set; } = new();
     public DateTime FinishDate { get; set; }
     public int YourPosition { get; set; }
+    public int YourScore { get; set; }
 }
 
 public record RankingEntry(string Username, int ProfileSkin, int Level, int Score);
